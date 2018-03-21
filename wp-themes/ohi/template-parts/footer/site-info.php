045c3b3d8991d1caca46b796ca9f37ec17e3c865
jnplonte <jnpl.onte@gmail.com>
2018-03-21 17:43:02 +0800

    add theme

---
+++ wp-themes/ohi/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage OHI
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ohi' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'ohi' ), 'WordPress' ); ?></a>
+</div><!-- .site-info -->
